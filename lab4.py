d15a328004f84d7e3c7e00dd5f0f386f2261836c
Vanek05042001 <86746347+Vanek05042001@users.noreply.github.com>
2024-10-15 18:58:23 +0300

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 22:20:52 2024
+
+@author: Vanya
+"""
+
+import sys
+sys.path.append('../')
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import cv2 as cv
+import numpy as np
+from utility import util
+
+plt.rcParams["figure.figsize"] = [6, 4]
+
+image1 = cv.imread('lab4.jpg')
+rgb_image1 = cv.cvtColor(image1, cv.COLOR_BGR2RGB)
+hsv_image1 = cv.cvtColor(image1, cv.COLOR_BGR2HSV)
+gray_image1 = cv.cvtColor(image1, cv.COLOR_BGR2GRAY)
+
+channels = [0]
+histSize = [256]
+range = [0, 256]
+
+gs = plt.GridSpec(2, 2)
+plt.figure(figsize=(10, 8))
+plt.subplot(gs[0])
+plt.imshow(image1, cmap='gray')
+plt.show()
+
+# Бинаризация
+threshold = 150
+image = image1
+
+ret, thresh1 = cv.threshold(image, threshold, 255, cv.THRESH_BINARY)
+
+plt.figure(figsize=(15, 8))
+
+plt.subplot(2, 3, 1)
+plt.imshow(thresh1, 'gray', vmin=0, vmax=255)
+plt.title('BINARY')
+plt.xticks([])
+plt.yticks([])
+plt.show()
+
+# Выделение границ
+outImageDepth = cv.CV_16S  
+
+gaussian33 = cv.GaussianBlur(thresh1, (3, 3), 0)
+gaussian55 = cv.GaussianBlur(thresh1, (5, 5), 0)
+
+laplace = cv.Laplacian(thresh1, outImageDepth, ksize=3)
+
+log = cv.Laplacian(gaussian33, outImageDepth, ksize=3)
+log = cv.convertScaleAbs(log)
+
+# Вывод
+plt.figure(figsize=(15, 8))
+gs = plt.GridSpec(1, 3)
+
+
+kernel1 = np.asarray([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+kernel2 = np.asarray([[-0.25, -0.25, -0.25], [-0.25, 3, -0.25], [-0.25, -0.25, -0.25]])
+kernel3 = np.asarray([[0, -0.25, 0], [-0.25, 2, -0.25], [0, -0.25, 0]])
+
+result_image = cv.filter2D(laplace, -1, kernel2)
+
+plt.subplot(gs[0])
+plt.xticks([]), plt.yticks([])
+plt.title('Результат')
+plt.imshow(result_image, cmap='gray')
+plt.show()
+
+# Сохранение в файл
+cv.imwrite('new.jpg', result_image)
